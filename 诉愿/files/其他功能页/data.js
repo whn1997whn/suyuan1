430380d245180c560c466628790efba4120c4a1f
whn1997whn <40093798+whn1997whn@users.noreply.github.com>
2018-06-09 17:15:49 +0800

    诉愿
    诉愿平台

---
+++ 诉愿/files/其他功能页/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i,j,k,l,m,n,o,p],q,_(r,s,t,u,v,w,x,_(),y,_(z,A,B,C,D,_(E,F,G,H),I,null,J,C,K,C,L,M,N,null,O,P,Q,R,S,T,U,P),V,_(),W,_(),X,_(Y,[])),Z,_(),ba,_());}; 
+var b="url",c="其他功能页.html",d="generationDate",e=new Date(1522590449325.54),f="isCanvasEnabled",g=false,h="variables",i="PU",j="sfz",k="sfz_n",l="wb",m="jishi",n="NewVariable",o="yema",p="yema2",q="page",r="packageId",s="5f656033711341e5a5bb00c6c3e53dd0",t="type",u="Axure:Page",v="name",w="其他功能页",x="notes",y="style",z="baseStyle",A="627587b6038d43cca051c114ac41ad32",B="pageAlignment",C="near",D="fill",E="fillType",F="solid",G="color",H=0xFFFFFFFF,I="image",J="imageHorizontalAlignment",K="imageVerticalAlignment",L="imageRepeat",M="auto",N="favicon",O="sketchFactor",P="0",Q="colorStyle",R="appliedColor",S="fontName",T="Applied Font",U="borderWidth",V="adaptiveStyles",W="interactionMap",X="diagram",Y="objects",Z="masters",ba="objectPaths";
+return _creator();
+})());
